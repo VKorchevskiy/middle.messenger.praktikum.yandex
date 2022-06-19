3c62fecbaaa2ab03fe0d5f2e374eca49419b7429
Vladimir Korchevskiy <burg2007@yandex.ru>
2022-06-20 00:18:02 +0300

    feat: добавить normalize.css, добавить кнопку, добавить инпут

---
+++ src/components/uikit/Button/index.js
@@ -0,0 +1,5 @@
+import Handlebars from "handlebars/dist/handlebars.runtime";
+
+import Button from "./button.hbs";
+
+Handlebars.registerPartial("Button", Button);
---
+++ src/components/uikit/Input/index.js
@@ -0,0 +1,5 @@
+import Handlebars from "handlebars/dist/handlebars.runtime";
+
+import Input from "./Input.hbs";
+
+Handlebars.registerPartial("Input", Input);
