225ba2de2c0afc0b5826186b68cefd1894ca054f
Vladimir Korchevskiy <burg2007@yandex.ru>
2022-06-22 01:19:37 +0300

    зарегистрировать компоненты

---
+++ src/utils/COMPONENTS.js
@@ -0,0 +1,13 @@
+import ErrorPage from "../components/layout/ErrorPage";
+import SignForm from "../components/layout/SignForm";
+import Button from "../components/uikit/Button";
+import Input from "../components/uikit/Input";
+
+const COMPONENTS = {
+  Button: Button,
+  Input: Input,
+  ErrorPage: ErrorPage,
+  SignForm: SignForm,
+};
+
+export default COMPONENTS;
