2231a89478a51b46195ac4c3b76a9941f4b4aa2b
Vladimir Korchevskiy <burg2007@yandex.ru>
2022-06-22 01:20:06 +0300

    шаблонизатор из практикума

---
+++ src/utils/Templator.js
@@ -0,0 +1,33 @@
+import getValue from "./getValue";
+import handlerTemplateValue from "./handlerTemplateValue";
+
+class Templator {
+  TEMPLATE_REGEXP = /\{\{(.*?)\}\}/gi;
+
+  constructor(template) {
+    this._template = template;
+  }
+
+  compile(context) {
+    return this._compileTemplate(context);
+  }
+
+  _compileTemplate = (context) => {
+    let template = this._template;
+    let key = null;
+    const regExp = this.TEMPLATE_REGEXP;
+
+    // Важно делать exec именно через константу, иначе уйдёте в бесконечный цикл
+    while ((key = regExp.exec(template))) {
+      if (key[1]) {
+        const templateValue = key[1].trim().split(" ");
+        const data = handlerTemplateValue(context, templateValue);
+        template = template.replace(new RegExp(key[0], "gi"), data);
+      }
+    }
+
+    return template;
+  };
+}
+
+export default Templator;
---
+++ src/utils/getValue.js
@@ -0,0 +1,16 @@
+function getValue(obj, path, defaultValue) {
+  const keys = path.split(".");
+
+  let result = obj;
+  for (let key of keys) {
+    result = result[key];
+
+    if (result === undefined) {
+      return defaultValue;
+    }
+  }
+
+  return result ?? defaultValue;
+}
+
+export default getValue;
