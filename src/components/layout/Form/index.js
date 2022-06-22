988b3b3e61ab2496a21646d8ab9436d073560925
Vladimir Korchevskiy <burg2007@yandex.ru>
2022-06-22 13:24:15 +0300

    компонент формы сделать общим для всех форм

---
+++ src/components/layout/Form/index.js
@@ -0,0 +1,23 @@
+import "./styles.scss";
+import getRenderedTemplate from "../../../utils/getRenderedTemplate";
+
+const FORM = `<form class="wrapper form">
+    <p class="form__title">
+      {{title}}
+    </p>
+    <div class="form__inputs">
+      {{#for inputs Input}}
+    </div>
+    <div class="form__actions">
+      {{#for buttons Button}}
+      {{#for links Link}}
+    </div>
+  </form>
+  `;
+
+function Form(props) {
+  //   const { title, link, button, inputs } = props;
+  return getRenderedTemplate(FORM, props);
+}
+
+export default Form;
