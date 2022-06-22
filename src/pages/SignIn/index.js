97a16a1141df68f923643d70c3103e1c801f92ac
Vladimir Korchevskiy <burg2007@yandex.ru>
2022-06-22 13:25:16 +0300

    форма авторизации

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,30 @@
-import Button from "../../components/uikit/Button";
-import Input from "../../components/uikit/Input";
+import SignForm from "../../components/layout/Form";
 
 const root = document.querySelector("#root");
+
+root.insertAdjacentHTML(
+  "afterbegin",
+  SignForm({
+    title: "Вход",
+    links: [{ href: "#", className: "", children: "Зарегистрироваться" }],
+    buttons: [{ type: "button", className: "", children: "Войти" }],
+    inputs: [
+      {
+        style: "marginBottom: 16px",
+        className: "",
+        name: "login",
+        labelText: "Логин",
+        type: "text",
+        errorText: "Ошибка",
+      },
+      {
+        style: "",
+        className: "",
+        name: "password",
+        labelText: "Пароль",
+        type: "password",
+        errorText: "Ошибка",
+      },
+    ],
+  })
+);
