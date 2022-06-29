afb091613db1486505cef430c1c35c9c38a2d212
Vladimir Korchevskiy <burg2007@yandex.ru>
2022-06-29 23:42:14 +0300

    feat(sprint_2): Добавить stilelint и поправить что-бы не было ошибок

--- src/components/layout/InWork/index.ts
+++ src/components/layout/InWork/index.ts
@@ -4,10 +4,10 @@ import getRenderedTemplate from "../../../utils/getRenderedTemplate";
 
 import { IInWork } from "./interface";
 
-const IN_WORK = `<section class="in_work">
-  <h1 class="text-xl in_work__title">Страница в работе</h1>
-  <p class="text-lg in_work__description">Будет доступна после следующего спринта</p>
-  <a class="text-xs in_work__link link" href="{{href}}">На разводную страницу</a>
+const IN_WORK = `<section class="in-work">
+  <h1 class="text-xl in-work__title">Страница в работе</h1>
+  <p class="text-lg in-work__description">Будет доступна после следующего спринта</p>
+  <a class="text-xs in-work__link link" href="{{href}}">На разводную страницу</a>
 </section>`;
 
 function InWork(props: IInWork) {
